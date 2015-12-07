e3e32c2426a72bbf296ad255ddb0061ecf4ed33a
Kaleb Elwert <belak@coded.io>
2015-12-07 13:19:15 -0800

    Change line iteration in day2

--- day2.py
+++ day2.py
@@ -1,26 +1,21 @@
 total = 0
 total_ribbon = 0
 
-with open('input/day2') as f:
-    while True:
-        line = f.readline()
-        if not line:
-            break
+for line in open('input/day2'):
+    l, w, h = line.split('x')
+    l, w, h = int(l), int(w), int(h)
 
-        l, w, h = line.split('x')
-        l, w, h = int(l), int(w), int(h)
+    sides = [l*w, l*h, w*h]
+    small = min(sides)
 
-        sides = [l*w, l*h, w*h]
-        small = min(sides)
+    #print('sides:', sides)
+    #print('smallest side area:', small)
+    #print('sum of sides:', sum(sides))
 
-        #print('sides:', sides)
-        #print('smallest side area:', small)
-        #print('sum of sides:', sum(sides))
+    ribbon_sides = [2*(l+w), 2*(l+h), 2*(w+h)]
+    total_ribbon += min(ribbon_sides) + l*w*h
 
-        ribbon_sides = [2*(l+w), 2*(l+h), 2*(w+h)]
-        total_ribbon += min(ribbon_sides) + l*w*h
-
-        total += (sum(sides) * 2 + small)
+    total += (sum(sides) * 2 + small)
 
 print('Wrapping paper needed:', total)
 print('Ribbon needed:', total_ribbon)
