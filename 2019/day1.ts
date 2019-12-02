a60288fb30617e32296c457f0ee625a921d99a59
Kaleb Elwert <belak@coded.io>
2019-12-01 19:43:55 -0800

    2019: day1: convert to an actual recursive function

--- 2019/day1.ts
+++ 2019/day1.ts
@@ -6,30 +6,24 @@ const lines = fs
   .readFileSync('input/day1')
   .toString()
   .trim()
-  .split('\n');
+  .split('\n')
+  .map((x) => +x);
 
 // Calculate the gas cost for a single item
 const calculate = (x: number) => Math.max(0, Math.floor(x / 3) - 2);
 
 // Given an initial gas cost, calculate the cost of the gas for this gas. Note
 // that this assumes the gas will be passed in and not the module weight.
-const recursiveCalculate = (x: number) => {
-  let ret = x;
-  let tmp = calculate(x);
+const recursiveCalculate = (x: number) =>
+  x === 0 ? x : x + recursiveCalculate(calculate(x));
 
-  while (tmp > 0) {
-    ret += tmp;
-    tmp = calculate(tmp);
-  }
-
-  return ret;
-}
-
-const moduleCosts = lines.map((line) => calculate(parseInt(line)));
-const modulesTotal = moduleCosts.reduce((total, current) => total + current, 0);
+const moduleCosts = lines.map(calculate);
+const modulesTotal = moduleCosts.reduce((total, current) => total + current);
 
 const recursiveCosts = moduleCosts.map(recursiveCalculate);
-const recursiveTotal = recursiveCosts.reduce((total, current) => total + current, 0)
+const recursiveTotal = recursiveCosts.reduce(
+  (total, current) => total + current
+);
 
 console.log(chalk.green('Total: ') + '%d', modulesTotal);
 console.log(chalk.green('Recursive Total: ') + '%d', recursiveTotal);
