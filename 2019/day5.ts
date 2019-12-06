ca0d667ed9ed0c62355eb3f20ad7acad1eddf166
Kaleb Elwert <belak@coded.io>
2019-12-05 17:21:32 -0800

    2019: day5: cleanup code

--- 2019/day5.ts
+++ 2019/day5.ts
@@ -9,107 +9,71 @@ const items = fs
   .split(',')
   .map((x: string) => +x);
 
-const processAdd = (
-  ptr: number,
-  data: number[],
-  input: number,
-  param1: number,
-  param2: number,
-  target: number
-): [number[], number, boolean] => [
-  data.map((item, index) => (index === target ? param1 + param2 : item)),
-  ptr + 4,
-  false,
-];
+const processAdd = (state: State, params: number[]): State => ({
+  ...state,
+  mem: state.mem.map((item, index) =>
+    index === params[2] ? params[0] + params[1] : item
+  ),
+  ptr: state.ptr + 4,
+});
 
-const processMul = (
-  ptr: number,
-  data: number[],
-  input: number,
-  param1: number,
-  param2: number,
-  target: number
-): [number[], number, boolean] => [
-  data.map((item, index) => (index === target ? param1 * param2 : item)),
-  ptr + 4,
-  false,
-];
+const processMul = (state: State, params: number[]): State => ({
+  ...state,
+  mem: state.mem.map((item, index) =>
+    index === params[2] ? params[0] * params[1] : item
+  ),
+  ptr: state.ptr + 4,
+});
 
-const processInp = (
-  ptr: number,
-  data: number[],
-  input: number,
-  target: number
-): [number[], number, boolean] => [
-  data.map((item, index) => (index === target ? input : item)),
-  ptr + 2,
-  false,
-];
+const processInp = (state: State, params: number[]): State => ({
+  ...state,
+  mem: state.mem.map((item, index) =>
+    index === params[0] ? state.input : item
+  ),
+  ptr: state.ptr + 2,
+});
 
-const processOut = (
-  ptr: number,
-  data: number[],
-  input: number,
-  target: number
-): [number[], number, boolean] => {
-  console.log('Out:', target);
-  return [data, ptr + 2, false];
+const processOut = (state: State, params: number[]): State => {
+  console.log('Out:', params[0]);
+
+  return {
+    ...state,
+    ptr: state.ptr + 2,
+  };
 };
 
-const processJT = (
-  ptr: number,
-  data: number[],
-  input: number,
-  param1: number,
-  target: number
-): [number[], number, boolean] => {
-  return [data, param1 !== 0 ? target : ptr + 3, false];
-};
+const processJT = (state: State, params: number[]): State => ({
+  ...state,
+  ptr: params[0] !== 0 ? params[1] : state.ptr + 3,
+});
 
-const processJF = (
-  ptr: number,
-  data: number[],
-  input: number,
-  param1: number,
-  target: number
-): [number[], number, boolean] => {
-  return [data, param1 === 0 ? target: ptr + 3, false];
-};
+const processJF = (state: State, params: number[]): State => ({
+  ...state,
+  ptr: params[0] === 0 ? params[1] : state.ptr + 3,
+});
 
-const processLT = (
-  ptr: number,
-  data: number[],
-  input: number,
-  param1: number,
-  param2: number,
-  target: number
-): [number[], number, boolean] => [
-  data.map((item, index) => (index === target ? (param1 < param2 ? 1 : 0) : item)),
-  ptr + 4,
-  false,
-];
+const processLT = (state: State, params: number[]): State => ({
+  ...state,
+  mem: state.mem.map((item, index) =>
+    index === params[2] ? (params[0] < params[1] ? 1 : 0) : item
+  ),
+  ptr: state.ptr + 4,
+});
 
-const processEq = (
-  ptr: number,
-  data: number[],
-  input: number,
-  param1: number,
-  param2: number,
-  target: number
-): [number[], number, boolean] => [
-  data.map((item, index) => (index === target ? (param1 === param2 ? 1 : 0) : item)),
-  ptr + 4,
-  false,
-];
+const processEq = (state: State, params: number[]): State => ({
+  ...state,
+  mem: state.mem.map((item, index) =>
+    index === params[2] ? (params[0] === params[1] ? 1 : 0) : item
+  ),
+  ptr: state.ptr + 4,
+});
 
-const processDone = (
-  ptr: number,
-  data: number[],
-  input: number,
-  target: number
-): [number[], number, boolean] => {
-  return [[], -1, true];
-};
+const processDone = (state: State, params: number[]): State => ({
+  ...state,
+  mem: [],
+  ptr: -1,
+  done: true,
+});
 
 function genericParamMapper(mode: string, data: number[], v: number) {
   switch (mode) {
@@ -122,15 +86,17 @@ function genericParamMapper(mode: string, data: number[], v: number) {
   throw new Error(`Unknown param mode: ${mode}`);
 }
 
-const immediateParamMapper = (mode: string, data: number[], v: number) => v;
+const immediateParamMapper: ParamMapper = (mode, data, v) => v;
 
-type OpCallback = (
-  ptr: number,
-  data: number[],
-  input: number,
-  ...params: number[]
-) => [number[], number, boolean];
+type OpCallback = (state: State, params: number[]) => State;
 type ParamMapper = (mode: string, data: number[], v: number) => number;
+
+type State = {
+  mem: number[];
+  ptr: number;
+  done: boolean;
+  input: number;
+};
 
 const paramMap: {
   [key: string]: [OpCallback, ParamMapper[]];
@@ -147,43 +113,55 @@ const paramMap: {
   '04': [processOut, [genericParamMapper]],
   '05': [processJT, [genericParamMapper, genericParamMapper]],
   '06': [processJF, [genericParamMapper, genericParamMapper]],
-  '07': [processLT, [genericParamMapper, genericParamMapper, immediateParamMapper]],
-  '08': [processEq, [genericParamMapper, genericParamMapper, immediateParamMapper]],
+  '07': [
+    processLT,
+    [genericParamMapper, genericParamMapper, immediateParamMapper],
+  ],
+  '08': [
+    processEq,
+    [genericParamMapper, genericParamMapper, immediateParamMapper],
+  ],
   '99': [processDone, []],
 };
 
 const processOpCode = (data: number[], input: number) => {
-  let ptr = 0;
-  let ret = data.slice();
-  let done = false;
+  let state: State = {
+    ptr: 0,
+    mem: data.slice(),
+    done: false,
+    input,
+  };
 
-  while (!done) {
-    const rawParamModes = ret[ptr]
+  while (!state.done) {
+    const rawParamModes = state.mem[state.ptr]
       .toString()
       .slice(0, -2)
       .split('')
       .reverse()
       .join('');
-    const opCode = ret[ptr]
+    const opCode = state.mem[state.ptr]
       .toString()
       .slice(-2)
       .padStart(2, '0');
 
     const fun = paramMap[opCode];
     if (!fun) {
-      throw new Error(`Unexpected opcode: ${opCode} (${ret[ptr]})`);
+      throw new Error(`Unexpected opcode: ${opCode} (${state.mem[state.ptr]})`);
     }
 
     const [cb, paramMappers] = fun;
     const paramCount = paramMappers.length;
     const paramModes = rawParamModes.padEnd(paramCount, '0');
-    const rawParams = ret.slice(ptr + 1, ptr + 1 + paramCount);
+    const rawParams = state.mem.slice(
+      state.ptr + 1,
+      state.ptr + 1 + paramCount
+    );
     const params = paramMappers.map((cb, i) =>
-      cb(paramModes[i], ret, rawParams[i])
+      cb(paramModes[i], state.mem, rawParams[i])
     );
 
     // console.log(opCode, params);
-    [ret, ptr, done] = cb(ptr, ret, input, ...params);
+    state = cb(state, params);
     // console.log('Ptr:', ptr)
   }
 
