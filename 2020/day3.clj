a152a06021626040a4fc4dcc429672ac21c90481
Kaleb Elwert <belak@coded.io>
2020-12-02 22:21:40 -0800

    2020: day 3: small clojure cleanup

--- 2020/day3.clj
+++ 2020/day3.clj
@@ -4,21 +4,23 @@
   (doall (line-seq r)))
 
 (defn slope [lines & {:keys [dx dy] :or {dx 3 dy 1}}]
-  (map
-   (fn [i]
-     (let [x (* i dx)
-           y (* i dy)
-           line (nth lines y)]
-       (nth line (rem x (count line)))))
-   (range (quot (count lines) dy))))
+  (count
+   (filter (fn [x] (= x \#))
+           (map
+            (fn [i]
+              (let [x (* i dx)
+                    y (* i dy)
+                    line (nth lines y)]
+                (nth line (rem x (count line)))))
+            (range (quot (count lines) dy))))))
 
 (let [lines (get-lines stdin)]
   ;; Part 1
-  (println (count (filter (fn [x] (= x \#)) (slope lines))))
+  (println (slope lines))
 
   (println (*
-            (count (filter (fn [x] (= x \#)) (slope lines :dx 1 :dy 1)))
-            (count (filter (fn [x] (= x \#)) (slope lines :dx 5 :dy 1)))
-            (count (filter (fn [x] (= x \#)) (slope lines :dx 7 :dy 1)))
-            (count (filter (fn [x] (= x \#)) (slope lines :dx 3 :dy 1)))
-            (count (filter (fn [x] (= x \#)) (slope lines :dx 1 :dy 2))))))
+            (slope lines :dx 1 :dy 1)
+            (slope lines :dx 5 :dy 1)
+            (slope lines :dx 7 :dy 1)
+            (slope lines :dx 3 :dy 1)
+            (slope lines :dx 1 :dy 2))))
