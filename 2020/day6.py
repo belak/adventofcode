967b94bd12a6a42889f81550d6556c33a81227da
Kaleb Elwert <belak@coded.io>
2020-12-05 21:15:36 -0800

    2020: day 6: initial commit

---
+++ 2020/day6.py
@@ -0,0 +1,39 @@
+lines = [
+    line.splitlines()
+    for line in open("day6-input").read().split('\n\n')
+]
+
+print(lines)
+
+
+def part1(data):
+    total = 0
+    for group in data:
+        group_questions = set()
+
+        for person in group:
+            for letter in person:
+                group_questions.add(letter)
+
+        total += len(group_questions)
+
+    return total
+
+
+def part2(data):
+    total = 0
+    for group in data:
+        group_questions = set(group[0])
+
+        for person in group:
+            for item in group[0]:
+                if item not in person:
+                    group_questions.discard(item)
+
+        total += len(group_questions)
+
+    return total
+
+
+print(part1(lines))
+print(part2(lines))
