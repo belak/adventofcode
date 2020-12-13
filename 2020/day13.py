2e0cf46793a3a14fee3ef07da8bd04a934533465
Kaleb Elwert <belak@coded.io>
2020-12-13 00:13:59 -0800

    2020: day 13: write a much more understandable solution

--- 2020/day13.py
+++ 2020/day13.py
@@ -1,3 +1,6 @@
+from itertools import count
+from functools import reduce
+
 lines = open("day13-input").read().splitlines()
 
 target = int(lines[0])
@@ -12,67 +15,72 @@ def part1(target, data):
     max_ts = 9999999999999
     for item in data:
         ts = item * (target // item + 1)
-        #print(item, ts - target)
         if ts < max_ts:
             max_ts_id = item
             max_ts = ts
-    #print(max_ts_id, max_ts, (max_ts - target))
+
     return max_ts_id * (max_ts - target)
 
 
-def is_valid(target, data):
-    #print(len(data))
-    if len(data) == 0:
-        return True
-
-    cur, rest = data[0], data[1:]
-    if cur == -1:
-        return is_valid(target + 1, rest)
-
-    return target % cur == 0 and is_valid(target + 1, rest)
-
-
-# Admittedly, chinese_remainder and mul_inv comes from Rosetta Code because I
-# have no patience for this kind of problem.
-from functools import reduce
-def chinese_remainder(n, a):
-    sum = 0
-    prod = reduce(lambda a, b: a*b, n)
-    for n_i, a_i in zip(n, a):
-        p = prod // n_i
-        sum += a_i * mul_inv(p, n_i) * p
-    return sum % prod
-
-
-def mul_inv(a, b):
-    b0 = b
-    x0, x1 = 0, 1
-    if b == 1: return 1
-    while a > 1:
-        q = a // b
-        a, b = b, a%b
-        x0, x1 = x1 - q * x0, x0
-    if x1 < 0: x1 += b0
-    return x1
-
-
 def part2(target, data):
-    values = []
-    remainders = []
+    # The goal is to reduce all values down to one. We do this by starting at
+    # what are essentially "identity" values and walking through the algorithm
+    # with each value. This can alternatively be done by starting with cur and
+    # step both set to data[0] (as well as iterating from index 1 in the list).
+    #
+    # For each offset/value combination, we find the point where the following
+    # formula is evenly divisible by the target value. When that's found, we
+    # update both cur and step.
+    #
+    # ```
+    # (cur + i * step + offset)
+    # ```
+    #
+    # Finally, we continue processing until there are no more values in the
+    # list.
 
-    for i in range(len(data)):
-        cur = data[i]
-        if cur == -1:
+    cur = 1
+    step = 1
+
+    for offset, val in enumerate(data):
+        # Skip timestamps where the schedule doesn't matter.
+        if val == -1:
             continue
 
-        # We actually can't use i directly because the remainder of start
-        # location is actually the inverse.
-        values += [cur]
-        remainders += [(cur-i)%cur]
+        # Loop through all starting points (cur + (i * step)) and check if this
+        # offset/value combo is valid. If yes, we can reduce and move on,
+        # otherwise continue looping.
+        #
+        # Cleverer people than me have used count(cur, step) (and resetting cur
+        # to the current count) because that's exactly what this does without as
+        # much code. I'm leaving this in its current form because this is how I
+        # wrote it and it works.
+        #
+        # Essentially, all of this block can be shortened to this:
+        #
+        # ```
+        # cur = next(p for p in count(cur, step) if (p + offset) % val == 0)
+        # ```
+        for i in count(1):
+            if (cur + (i * step) + offset) % val == 0:
+                cur += (i * step)
+                break
 
-    return chinese_remainder(values, remainders)
+        # My first attempt used cur and step as the same value, but this isn't
+        # valid. I thought this would work because we can validly start with
+        # data[0] as both cur and step. Unfortunately, we need to use the
+        # product of all processed values up to this point as the step.
+        #
+        # My second attempt stored all completed values and multiplied them
+        # together every time.
+        #
+        # This final version just keeps the running total of all the multiplied
+        # values.
+        step *= val
+
+    return cur
 
 
-print(part1(target, list(filter(lambda x: x != -1, lines))))
-print(part2(target, lines))
+print('part1:', part1(target, list(filter(lambda x: x != -1, lines))))
+print('part2:', part2(target, lines))
 
