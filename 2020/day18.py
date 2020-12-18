8a226f68345756583768b16938a96bab118d0bff
Kaleb Elwert <belak@coded.io>
2020-12-17 22:24:58 -0800

    2020: day 18: add comments

--- 2020/day18.py
+++ 2020/day18.py
@@ -1,3 +1,6 @@
+# NOTE: most days we don't massage the data like this, but I didn't want to deal
+# with parsing out open/close parens inside existing items. By adding spaces, we
+# can handle them exactly the same as normal tokens.
 data = [
     line.split(" ")
     for line in open("day18-input")
@@ -20,13 +23,25 @@ class Calculator:
 
     def parse_value(self, data):
         item, rest = data[0], data[1:]
+
+        # A value can only be either an open paren or a value.
         if item == "(":
+            # For open paren, we split the expression into 2 pieces: the portion
+            # inside the parens, and what comes after.
             expr, rest = self.split_inner_expr(rest)
+
+            # Now that it's split, we recurse and try to handle the expression
+            # that was inside these parens. Because we properly reduce it to a
+            # value, we're still returning a single value here (followed by the
+            # rest of the equation).
             return self.parse_expr(expr), rest
         else:
             return int(item), rest
 
     def split_inner_expr(self, line):
+        # This takes the remainder of a line right after an open paren and
+        # splits it into the portion inside the parens (handling nested parens
+        # properly), and what comes after the parenthetical expression.
         depth = 1
         i = 0
         for item in line:
@@ -42,7 +57,10 @@ class Calculator:
         raise Exception("mismatched parens")
 
     def parse_expr(self, data):
-        # Start by flattening out this array - calculate any nested parens as a value.
+        # Start by flattening out this array - calculate any nested parens as a
+        # value. This will leave us with an array in the form of [value, op,
+        # value, ..., op, value]. We can take the array in that form and reduce
+        # it by running passes on it for each stage of operators.
         total, rest = self.parse_value(data)
         tmp = [total]
         while len(rest) > 0:
@@ -51,6 +69,8 @@ class Calculator:
             tmp += [arg, val]
         rest = tmp
 
+        # For mode 1, all operations are the same level, while for mode 2,
+        # they're separate and in a specific order.
         if self.mode == 1:
             precidence = ["+*"]
         else:
@@ -60,18 +80,27 @@ class Calculator:
         # reduce the array. When we get to the end, we should have 1 element
         # in the list.
         for valid_ops in precidence:
-            i = 0
-            while (2 * i) + 1 < len(rest):
-                idx = (2 * i) + 1
-                op = rest[idx]
+            # We want to loop through the operators and make decisions based on
+            # those.
+            i = 1
+            while i < len(rest):
+                op = rest[i]
                 if op in valid_ops:
-                    rest = (
-                        rest[: idx - 1]
-                        + [Calculator.OPS[op](rest[idx - 1], rest[idx + 1])]
-                        + rest[idx + 2 :]
-                    )
+                    # For every valid op, we split the values into 3 parts:
+                    #
+                    # - What came before this operation
+                    # - The set of 3 items which make up an operation (value, op, value)
+                    # - What comes after this operation
+
+                    prev = rest[: i - 1]
+                    calc = Calculator.OPS[op](rest[i - 1], rest[i + 1])
+                    post = rest[i + 2 :]
+
+                    rest = prev + [calc] + post
                 else:
-                    i += 1
+                    # If this wasn't a valid op for this phase, move on to the
+                    # next value.
+                    i += 2
 
         if len(rest) != 1:
             print(rest)
