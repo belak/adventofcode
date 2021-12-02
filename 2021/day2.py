39825bc7b972cfce177d5c3dd6e6736a5dd64c2e
Kaleb Elwert <belak@coded.io>
2021-12-01 21:07:21 -0800

    2021: day 2: initial commit

---
+++ 2021/day2.py
@@ -0,0 +1,34 @@
+data = [line.split() for line in open('day2-input').read().splitlines()]
+data = [(line[0], int(line[1])) for line in data]
+
+depth = 0
+pos = 0
+
+for (direction, amount) in data:
+    if direction == "forward":
+        pos += amount
+    elif direction == "down":
+        depth += amount
+    elif direction == "up":
+        depth -= amount
+    else:
+        raise Exception("unknown direction: %s" % direction)
+
+print(depth*pos)
+
+depth = 0
+pos = 0
+aim = 0
+
+for (direction, amount) in data:
+    if direction == "forward":
+        pos += amount
+        depth += (aim * amount)
+    elif direction == "down":
+        aim += amount
+    elif direction == "up":
+        aim -= amount
+    else:
+        raise Exception("unknown direction: %s" % direction)
+
+print(depth*pos)
