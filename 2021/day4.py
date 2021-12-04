dd9f6901bb433848f55c81379f3d5da7b82c2287
Kaleb Elwert <kaleb.elwert@datadoghq.com>
2021-12-04 01:01:39 -0800

    2021: day 4: code golf with numpy

--- 2021/day4.py
+++ 2021/day4.py
@@ -1,68 +1,34 @@
-data = open('day4-input').read().split('\n\n')
+import numpy
 
-numbers = map(int, data[0].split(','))
-boards = []
+data = open("day4-input").read().split("\n\n")
 
-for board in data[1:]:
-    boards.append([list(map(int, line.split())) for line in board.splitlines()])
+numbers = map(int, data[0].split(","))
+boards = [
+    numpy.array(list(list(map(int, line.split())) for line in board.splitlines()))
+    for board in data[1:]
+]
 
 def board_complete(board):
-    # Check rows
-    for line in board:
-        if line.count(None) == len(line):
-            return True
-
-    # Check cols
-    for x in range(len(board[0])):
-        valid = True
-        for line in board:
-            if line[x] != None:
-                valid = False
-                break
-
-        if valid:
-            return True
-
-    return False
-
-def total_board(board):
-    return sum([sum(filter(lambda x: x is not None, line)) for line in board])
+    return 0 in board.sum(axis=0) or 0 in board.sum(axis=1)
 
 def mark_number(boards, number):
-    complete = None
+    complete = []
+    incomplete = []
 
     for idx in range(len(boards)):
-        boards[idx] = [
-            list(map(lambda x: x if x != number else None, line)) for line in boards[idx]
-        ]
-
-        board = boards[idx]
+        board = numpy.where(boards[idx] == number, 0, boards[idx])
 
         if board_complete(board):
-            complete = board
+            complete.append(board)
+        else:
+            incomplete.append(board)
 
-    if complete is not None:
-        total = total_board(complete)
-        return boards, number * total
+    return complete, incomplete
 
-    return boards, None
-
-def mark_number2(boards, number):
-    for idx in range(len(boards)):
-        boards[idx] = [
-            list(map(lambda x: x if x != number else None, line)) for line in boards[idx]
-        ]
-
-    return boards, [board for board in boards if not board_complete(board)]
-
+# NOTE: We really want the scores of completed boards - the first one is part 1,
+# the last one is part 2.
+incomplete = boards
 for number in numbers:
-    boards, x = mark_number(boards, number)
-    if x is not None:
-        print(x)
-        break
-
-for number in numbers:
-    all_boards, boards = mark_number2(boards, number)
-    if len(all_boards) == 1 and len(boards) == 0:
-        print(total_board(all_boards[0]) * number)
-        break
+    complete, incomplete = mark_number(incomplete, number)
+    for board in complete:
+        print(board.sum() * number)
