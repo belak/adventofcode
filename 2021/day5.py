0c7ae67a8c58e1b04dbd989d4baf59bbca0af033
Kaleb Elwert <belak@coded.io>
2021-12-04 22:02:26 -0800

    2021: day 5: cleanup

--- 2021/day5.py
+++ 2021/day5.py
@@ -1,77 +1,36 @@
-from dataclasses import dataclass
-
-@dataclass
-class Point:
-    x: int
-    y: int
-
+from numpy import sign  # why is this not in the stdlib
 
 data = [line.split(" -> ") for line in open("day5-input").read().splitlines()]
 data = [[elem.split(",") for elem in item] for item in data]
 data = [[list(map(int, elem)) for elem in item] for item in data]
-data = [[Point(x=elem[0], y=elem[1]) for elem in item] for item in data]
 
-output = {}
 
-for line in data:
-    # Skip non horizontal/vertical lines
-    if line[0].x != line[1].x and line[0].y != line[1].y:
-        continue
+def calc(skip_func):
+    output = {}
 
-    dx = 1
-    dy = 1
-    if line[0].x > line[1].x:
-        dx = -1
+    for line in data:
+        if skip_func(line):
+            continue
 
-    if line[0].y > line[1].y:
-        dy = -1
+        distance_x = line[1][0] - line[0][0]
+        distance_y = line[1][1] - line[0][1]
 
-    for x in range(line[0].x, line[1].x + dx, dx):
-        for y in range(line[0].y, line[1].y + dy, dy):
-            key = (x, y)
-            # print(key)
+        dx = sign(distance_x)
+        dy = sign(distance_y)
+
+        distance = max(abs(distance_x), abs(distance_y))
+
+        for i in range(distance + 1):
+            key = (line[0][0] + i * dx, line[0][1] + i * dy)
             if key not in output:
                 output[key] = 0
             output[key] += 1
 
-# print(output)
+    return output
 
+
+output = calc(lambda line: line[0][0] != line[1][0] and line[0][1] != line[1][1])
 print(len(list(filter(lambda x: x >= 2, output.values()))))
 
-output = {}
-
-for line in data:
-    distance_x = line[0].x - line[1].x
-    distance_y = line[0].y - line[1].y
-
-    if distance_x < 0:
-        dx = 1
-    elif distance_x > 0:
-        dx = -1
-    else:
-        dx = 0
-
-    if distance_y < 0:
-        dy = 1
-    elif distance_y > 0:
-        dy = -1
-    else:
-        dy = 0
-
-    distance = max(abs(distance_x), abs(distance_y))
-    print(line[0], line[1], distance)
-
-    for i in range(distance + 1):
-        key = (line[0].x + i * dx, line[0].y + i * dy)
-        print(key)
-        if key not in output:
-            output[key] = 0
-        output[key] += 1
-
-for y in range(10):
-    for x in range(10):
-        print(output.get((x, y), "."), end="")
-    print()
-
-
+output = calc(lambda _: False)
 print(len(list(filter(lambda x: x >= 2, output.values()))))
