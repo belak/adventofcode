a5aaf841ab7c9b1ccc33080ac11bea08978a2366
Kaleb Elwert <belak@coded.io>
2021-12-05 21:33:50 -0800

    2021: day 6: cleanup

--- 2021/day6.py
+++ 2021/day6.py
@@ -1,28 +1,14 @@
 data = list(map(int, open("day6-input").read().split(',')))
 
-timers = data.copy()
-
-for i in range(80):
-    # Step
-    for i in range(len(timers)):
-        if timers[i] == 0:
-            timers.append(8)
-            timers[i] = 7
-        timers[i] -= 1
-
-print(len(timers))
-
-timers = {i: 0 for i in range(9)}
-for item in data:
-    timers[item] += 1
+timers = [data.count(i) for i in range(9)]
 
 for i in range(256):
-    zero_timers = timers[0]
+    # Day 1 asks for the answer after 80 days, but it's the same formula.
+    if i == 80:
+        print(sum(timers))
 
-    for j in range(8):
-        timers[j] = timers[j+1]
-
+    zero_timers = timers.pop(0)
+    timers.append(zero_timers)
     timers[6] += zero_timers
-    timers[8] = zero_timers
 
-print(sum(timers.values()))
+print(sum(timers))
