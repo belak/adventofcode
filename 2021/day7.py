3ad055a904eb8ecfc34431db699db53b9f7da4c3
Kaleb Elwert <belak@coded.io>
2021-12-06 21:11:35 -0800

    2021: day 7: initial commit

---
+++ 2021/day7.py
@@ -0,0 +1,30 @@
+data = list(map(int, open("day7-input").read().split(",")))
+
+min_crab = min(data)
+max_crab = max(data)
+
+min_total = 9999999
+min_part2_total = 99999999
+
+for x in range(min_crab, max_crab + 1):
+    total = 0
+    part2_total = 0
+
+    for crab in data:
+        crab_total = abs(crab - x)
+
+        # These are triangular numbers, so there's a constant-time calculation
+        # for it: https://www.mathsisfun.com/algebra/triangular-numbers.html
+        part2_crab_total = int(crab_total * (crab_total + 1) / 2)
+
+        total += crab_total
+        part2_total += part2_crab_total
+
+    if total < min_total:
+        min_total = total
+
+    if part2_total < min_part2_total:
+        min_part2_total = part2_total
+
+print(min_total)
+print(min_part2_total)
