2605c73eda6e69e11b74fa57ced5c32aaba7fd5e
Kaleb Elwert <belak@coded.io>
2021-12-10 21:59:38 -0800

    2021: day 11: initial commit

---
+++ 2021/day11.py
@@ -0,0 +1,63 @@
+import numpy as np
+
+dirs = [
+    (0, 1),
+    (0, -1),
+    (1, 0),
+    (1, -1),
+    (1, 1),
+    (-1, 0),
+    (-1, 1),
+    (-1, -1),
+]
+
+data = open("day11-input").read().splitlines()
+data = np.array([list(map(int, line)) for line in data])
+
+count = 0
+
+i = 0
+while True:
+    # This feels like cheating, thanks numpy
+    data += 1
+
+    flashed = set()
+
+    items = np.where(data > 9)
+    items = set(zip(items[0], items[1]))
+    while len(items - flashed) > 0:
+        for coord in items:
+            if coord in flashed:
+                continue
+
+            flashed.add(coord)
+
+            for dx, dy in dirs:
+                new_x = coord[1] + dx
+                new_y = coord[0] + dy
+
+                if new_x < 0 or new_y < 0:
+                    continue
+
+                try:
+                    data[new_y, new_x] += 1
+                except Exception as e:
+                    pass
+
+        items = np.where(data > 9)
+        items = set(zip(items[0], items[1]))
+
+    for item in flashed:
+        data[item[0], item[1]] = 0
+
+    if i < 100:
+        count += len(flashed)
+    else:
+        if np.all(data == 0):
+            break
+
+    i += 1
+
+
+print(count)
+print(i + 1)
