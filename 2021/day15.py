3b408bad9c86d847dd7cfbfaa0d1e65c80e8765b
Kaleb Elwert <belak@coded.io>
2021-12-14 21:53:49 -0800

    2021: day 15: use same graph code for both parts

--- 2021/day15.py
+++ 2021/day15.py
@@ -11,47 +11,52 @@ dirs = [
     (-1, 0),
 ]
 
-G = nx.DiGraph()
 
-G.add_node("start")
-G.add_node("end")
+def populate_grid(data, G=None, multiplier=1, skip_mult=0):
+    G = nx.DiGraph()
+    G.add_node("start")
+    G.add_edge("start", (0, 0), weight=0)
+    G.add_edge(
+        (len(data[0]) * multiplier - 1, len(data) * multiplier - 1),
+        "end",
+        weight=0,
+    )
 
-for y in range(len(data) * 5):
-    for x in range(len(data[0]) * 5):
-        pos = (x, y)
-        G.add_node(pos)
+    for y in range(len(data) * multiplier):
+        for x in range(len(data[0]) * multiplier):
+            pos = (x, y)
+            G.add_node(pos)
 
-        for (dx, dy) in dirs:
-            neighbor_pos = (x + dx, y + dy)
-            if neighbor_pos[0] < 0 or neighbor_pos[0] >= len(data[0]) * 5:
-                continue
-            if neighbor_pos[1] < 0 or neighbor_pos[1] >= len(data) * 5:
-                continue
+            for (dx, dy) in dirs:
+                neighbor_pos = (x + dx, y + dy)
+                if neighbor_pos[0] < 0 or neighbor_pos[0] >= len(data[0]) * multiplier:
+                    continue
+                if neighbor_pos[1] < 0 or neighbor_pos[1] >= len(data) * multiplier:
+                    continue
 
-            neighbor_grid_loc = (
-                neighbor_pos[0] // len(data[0]),
-                neighbor_pos[1] // len(data),
-            )
-            neighbor_sub_pos = (
-                neighbor_pos[0] % len(data[0]),
-                neighbor_pos[1] % len(data),
-            )
-            weight = (neighbor_grid_loc[0] + neighbor_grid_loc[1]) + data[
-                neighbor_sub_pos[1]
-            ][neighbor_sub_pos[0]]
-            if weight > 9:
-                weight -= 9
+                neighbor_grid_loc = (
+                    neighbor_pos[0] // len(data[0]),
+                    neighbor_pos[1] // len(data),
+                )
+                neighbor_sub_pos = (
+                    neighbor_pos[0] % len(data[0]),
+                    neighbor_pos[1] % len(data),
+                )
+                weight = (neighbor_grid_loc[0] + neighbor_grid_loc[1]) + data[
+                    neighbor_sub_pos[1]
+                ][neighbor_sub_pos[0]]
+                if weight > 9:
+                    weight -= 9
 
-            G.add_edge(pos, neighbor_pos, weight=weight)
+                G.add_edge(pos, neighbor_pos, weight=weight)
 
-G1 = G.copy()
-G1.add_edge("start", (0, 0), weight=0)
-G1.add_edge((len(data[0]) - 1, len(data) - 1), "end", weight=0)
-path = nx.shortest_path(G1, "start", "end", "weight")
-print(path_weight(G1, path, "weight"))
+    return G
 
-G2 = G.copy()
-G2.add_edge("start", (0, 0), weight=0)
-G2.add_edge((len(data[0]) * 5 - 1, len(data) * 5 - 1), "end", weight=0)
-path = nx.shortest_path(G2, "start", "end", "weight")
-print(path_weight(G2, path, "weight"))
+
+G = populate_grid(data)
+path = nx.shortest_path(G, "start", "end", "weight")
+print(path_weight(G, path, "weight"))
+
+G = populate_grid(data, G, multiplier=5, skip_mult=1)
+path = nx.shortest_path(G, "start", "end", "weight")
+print(path_weight(G, path, "weight"))
