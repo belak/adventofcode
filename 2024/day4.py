416c049144f03e601031a2fe86ed03e635615d19
Kaleb Elwert <belak@coded.io>
2024-12-03 21:36:46 -0800

    2024: day4: initial commit

---
+++ 2024/day4.py
@@ -0,0 +1,59 @@
+import re
+
+data = [line for line in open('./input-4').readlines()]
+
+total = 0
+total2 = 0
+
+allowed_words = ["XMAS", "SAMX"]
+
+for i in range(len(data)):
+    for j in range(len(data[i])):
+        # Horizontal
+        try:
+            check = data[i][j] + data[i][j+1] + data[i][j+2] + data[i][j+3]
+            if check in allowed_words:
+                total += 1
+        except IndexError:
+            pass
+
+        # Vertical
+        try:
+            check = data[i][j] + data[i+1][j] + data[i+2][j] + data[i+3][j]
+            if check in allowed_words:
+                total += 1
+        except IndexError:
+            pass
+
+        # Diagonal
+        try:
+            check = data[i][j] + data[i+1][j+1] + data[i+2][j+2] + data[i+3][j+3]
+            if check in allowed_words:
+                total += 1
+        except IndexError:
+            pass
+
+        try:
+            check = data[i][j] + data[i+1][j-1] + data[i+2][j-2] + data[i+3][j-3]
+            if check in allowed_words:
+                total += 1
+        except IndexError:
+            pass
+
+allowed_diags = ["MAS", "SAM"]
+
+for i in range(len(data)):
+    for j in range(len(data[i])):
+        if data[i][j] != 'A':
+            continue
+
+        try:
+            d1 = data[i-1][j-1] + data[i][j] + data[i+1][j+1]
+            d2 = data[i-1][j+1] + data[i][j] + data[i+1][j-1]
+            if d1 in allowed_diags and d2 in allowed_diags:
+                total2 += 1
+        except IndexError:
+            pass
+
+print(total)
+print(total2)
